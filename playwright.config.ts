9b35072756327328cfbfa3efcba9cc467a1301ca
Cosmin <cosmin0898@yahoo.com>
2026-07-20 15:53:05 +0300

    add: added folder structure and project config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,10 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  timeout: 180_000, // timeout in milliseconds for ALL tests (180 seconds / 3minutes)
+  expect: {timeout: 30_000}, // timeout for individual expect() assertions (30 seconds)
+  testDir: './features',
+  snapshotPathTemplate: "{testDir}/{testFileDir}/snapshots/{testFileName}-snapshots/{arg}{ext}",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -39,15 +42,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
